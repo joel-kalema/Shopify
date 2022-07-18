ed6ed0a14fcc2d97f94b0d65e7cfb082d39f9655
joel <joelkalema63@gmail.com>
2022-07-18 18:59:09 +0200

    stup the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {data} from './redux/actons/data'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>hello ward</h1>
+    </div>
+  );
+}
+
+export default App;
